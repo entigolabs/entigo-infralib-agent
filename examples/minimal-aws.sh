9aa9c43d29cf658a9ef559274900320af4dddd46
martivo <martin.vool@entigo.com>
2024-10-16 14:12:49 +0300

    Update examples

--- examples/minimal-aws.sh
+++ examples/minimal-aws.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export AWS_REGION="eu-north-1"
-docker run --pull always -it --rm -v "$(pwd)/minimal-aws.yaml":"/etc/ei-agent/config.yaml" -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_REGION=$AWS_REGION -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN -e CONFIG=/etc/ei-agent/config.yaml entigolabs/entigo-infralib-agent
+docker run --pull always -it --rm -v "$(pwd)":"/etc/ei-agent/conf" -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_REGION=$AWS_REGION -e AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN -e CONFIG=/etc/ei-agent/conf/minimal-aws.yaml entigolabs/entigo-infralib-agent
--- examples/minimal-google.sh
+++ examples/minimal-google.sh
@@ -3,4 +3,4 @@ export LOCATION="europe-north1"
 export ZONE="europe-north1-a"
 export PROJECT_ID="entigo-infralib-agent"
 
-docker run --pull always -it --rm -v $(echo ~)/.config/gcloud:/root/.config/gcloud -v "$(pwd)/minimal-google.yaml":"/etc/ei-agent/config.yaml" -e PROJECT_ID=$PROJECT_ID -e LOCATION=$LOCATION -e ZONE=$ZONE -e CONFIG=/etc/ei-agent/config.yaml entigolabs/entigo-infralib-agent
+docker run --pull always -it --rm -v $(echo ~)/.config/gcloud:/root/.config/gcloud -v "$(pwd)":"/etc/ei-agent/conf" -e PROJECT_ID=$PROJECT_ID -e LOCATION=$LOCATION -e ZONE=$ZONE -e CONFIG=/etc/ei-agent/conf/minimal-google.yaml entigolabs/entigo-infralib-agent
