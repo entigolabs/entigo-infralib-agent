26a621480df655cd42e44973045bdc076735d3d5
Märt Erlenheim <mart.erlenheim@entigo.com>
2024-12-03 14:19:00 +0200

    Increase wait time for gcloud SA to avoid not exist error

--- gcloud/gcloud.go
+++ gcloud/gcloud.go
@@ -181,7 +181,7 @@ func (g *gcloudService) createServiceAccount(iam *IAM) string {
 	if err != nil {
 		log.Fatalf("Failed to create service account: %s", err)
 	}
-	time.Sleep(1 * time.Second) // Adding roles immediately after account creation may fail with SA does not exist
+	time.Sleep(2 * time.Second) // Adding roles immediately after account creation may fail with SA does not exist
 	err = iam.AddRolesToServiceAccount(account.Name, []string{"roles/editor", "roles/iam.securityAdmin",
 		"roles/iam.serviceAccountAdmin"})
 	if err != nil {
@@ -233,7 +233,7 @@ func (g *gcloudService) CreateServiceAccount() {
 		log.Printf("Service account %s already exists\n", account.Name)
 		return
 	}
-	time.Sleep(1 * time.Second) // Adding roles immediately after account creation may fail with SA does not exist
+	time.Sleep(2 * time.Second) // Adding roles immediately after account creation may fail with SA does not exist
 	err = iam.AddRolesToServiceAccount(account.Name, []string{"roles/editor", "roles/iam.securityAdmin"})
 	if err != nil {
 		log.Fatalf("Failed to add roles to service account: %s", err)
