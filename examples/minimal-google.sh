49abbe4434dd25e18533fd28c0fca4c9ac96213d
taivox <taivox@gmail.com>
2024-12-09 14:40:55 +0200

    Update google example conf

--- examples/minimal-google.sh
+++ examples/minimal-google.sh
@@ -3,4 +3,10 @@ export LOCATION=""
 export ZONE=""
 export PROJECT_ID=""
 
-docker run --pull always -it --rm -v $(echo ~)/.config/gcloud:/root/.config/gcloud -v "$(pwd)":"/etc/ei-agent/conf" -e PROJECT_ID=$PROJECT_ID -e LOCATION=$LOCATION -e ZONE=$ZONE -e CONFIG=/etc/ei-agent/conf/minimal-google.yaml entigolabs/entigo-infralib-agent
+docker run --pull always -it --rm \
+    -v $(echo ~)/.config/gcloud:/root/.config/gcloud \
+    -v "$(pwd)":"/etc/ei-agent/config/" \
+    -e PROJECT_ID=$PROJECT_ID \
+    -e LOCATION=$LOCATION \
+    -e ZONE=$ZONE \
+    -e CONFIG=/etc/ei-agent/config/minimal-google.yaml entigolabs/entigo-infralib-agent
