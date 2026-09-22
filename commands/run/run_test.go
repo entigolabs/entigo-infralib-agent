80b18b8ea2d06d62d908b432310a926458454093
Märt Erlenheim <mart.erlenheim@entigo.com>
2026-09-22 12:02:51 +0300

    Added Oracle e2e test

--- commands/run/run_test.go
+++ commands/run/run_test.go
@@ -1,11 +1,11 @@
 package run
 
 import (
-	"context"
 	"os"
 	"testing"
 
 	"github.com/entigolabs/entigo-infralib-agent/common"
+	"github.com/entigolabs/entigo-infralib-agent/model"
 	"github.com/entigolabs/entigo-infralib-agent/service"
 	"github.com/entigolabs/entigo-infralib-agent/test"
 )
@@ -31,10 +31,10 @@ func TestRunAWS(t *testing.T) {
 			Type: string(common.PipelineTypeCloud),
 		},
 	}
-	if err := Run(context.Background(), flags); err != nil {
+	if err := Run(t.Context(), flags); err != nil {
 		t.Fatalf("failed to run: %v", err)
 	}
-	deleter, err := service.NewDeleter(context.Background(), flags)
+	deleter, err := service.NewDeleter(t.Context(), flags)
 	if err != nil {
 		t.Fatalf("failed to create deleter: %v", err)
 	}
@@ -75,10 +75,52 @@ func TestRunGCloud(t *testing.T) {
 			Type: string(common.PipelineTypeCloud),
 		},
 	}
-	if err := Run(context.Background(), flags); err != nil {
+	if err := Run(t.Context(), flags); err != nil {
 		t.Fatalf("failed to run: %v", err)
 	}
-	deleter, err := service.NewDeleter(context.Background(), flags)
+	deleter, err := service.NewDeleter(t.Context(), flags)
+	if err != nil {
+		t.Fatalf("failed to create deleter: %v", err)
+	}
+	if err := deleter.Destroy(); err != nil {
+		t.Fatalf("failed to destroy: %v", err)
+	}
+	if err := deleter.Delete(); err != nil {
+		t.Fatalf("failed to delete: %v", err)
+	}
+}
+
+func TestRunOracle(t *testing.T) {
+	t.Parallel()
+	if err := common.ChooseLogger(string(common.DebugLogLevel)); err != nil {
+		t.Fatalf("failed to choose logger: %v", err)
+	}
+	test.ChangeRunDir()
+	prefix := os.Getenv(common.AwsPrefixEnv)
+	if len(prefix) > 10 {
+		prefix = prefix[:10]
+	}
+	flags := &common.Flags{
+		Config: "test/profile-oracle.yaml",
+		Prefix: prefix,
+		Oracle: common.Oracle{
+			Region:        os.Getenv(model.OracleRegion),
+			CompartmentId: os.Getenv(common.OracleCompartmentIdEnv),
+			Profile:       os.Getenv(common.OracleProfileEnv),
+			ConfigFile:    os.Getenv(common.OracleConfigFileEnv),
+		},
+		SkipBucketCreationDelay: true,
+		Delete: common.DeleteFlags{
+			DeleteBucket: true,
+		},
+		Pipeline: common.Pipeline{
+			Type: string(common.PipelineTypeCloud),
+		},
+	}
+	if err := Run(t.Context(), flags); err != nil {
+		t.Fatalf("failed to run: %v", err)
+	}
+	deleter, err := service.NewDeleter(t.Context(), flags)
 	if err != nil {
 		t.Fatalf("failed to create deleter: %v", err)
 	}
